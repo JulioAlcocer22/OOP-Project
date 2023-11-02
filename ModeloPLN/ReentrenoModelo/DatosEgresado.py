0cae537461280aa26f393e27d128b66bf1a2a32a
JulioAlcocer22 <a18001293@alumnos.uady.mx>
2023-11-02 10:39:52 -0600

    Agregacion de clase, correcion de codigo

---
+++ ModeloPLN/ReentrenoModelo/DatosEgresado.py
@@ -0,0 +1,11 @@
+class Egresado:
+    nombre = "Juan"
+    empresa = "Amazon"
+    rol = "Web Developer"
+    descripcion = "Descricpion generica"
+
+    def __init__(self, nombre, empresa, rol, descripcion):
+        self.nombre = nombre
+        self.empresa = empresa
+        self.rol = rol
+        self.descripcion = descripcion
--- ModeloPLN/ReentrenoModelo/Tokenizador.py
+++ ModeloPLN/ReentrenoModelo/Tokenizador.py
@@ -1,32 +1,40 @@
 from transformers import BertTokenizer
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+class Tokenizador:
 
-sentences = [
-    "Esta es la primera oración.",
-    "Esta es la segunda oración.",
-    "Esta es la tercera oración."
-]
+    def __init__(self, sentences):
+        self.sentences = sentences
 
-# Lista para almacenar las representaciones tokenizadas de las oraciones
-tokenized_sentences = []
 
-for sentence in sentences:
-    # Utiliza el tokenizador para procesar cada oración
-    tokens = tokenizer(
-        sentence,
-        padding='max_length',
-        max_length=128,
-        truncation=True,
-        return_tensors="pt"
-    )
+    sentences = [
+        "Esta es la primera oración.",
+        "Esta es la segunda oración.",
+        "Esta es la tercera oración."
+    ]
 
-    # Agrega la representación tokenizada a la lista
-    tokenized_sentences.append(tokens)
+    def tokenizar(self, sentences):
+        #cargando modelo
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-# Ahora, tokenized_sentences contiene las representaciones tokenizadas de todas las oraciones en tu arreglo.
-print("Proceso finalizado")
+        # Lista para almacenar las representaciones tokenizadas de las oraciones
+        tokenized_sentences = []
 
-for i in tokenized_sentences:
-    print(i)
-    print("\n\n")
+        for sentence in sentences:
+            # Utiliza el tokenizador para procesar cada oración
+            tokens = tokenizer(
+                sentence,
+                padding='max_length',
+                max_length=128,
+                truncation=True,
+                return_tensors="pt"
+            )
+
+            # Agrega la representación tokenizada a la lista
+            tokenized_sentences.append(tokens)
+
+        # Ahora, tokenized_sentences contiene las representaciones tokenizadas de todas las oraciones en tu arreglo.
+        print("Proceso finalizado")
+
+        for i in tokenized_sentences:
+            print(i)
+            print("\n\n")
