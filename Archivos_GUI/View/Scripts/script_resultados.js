0143f22b45a67f677acbb39f441903f606989a7a
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2023-12-10 13:24:49 -0600

    Archivos_Vista_Resultados

--- Archivos_GUI/View/Scripts/script_resultados.js
+++ Archivos_GUI/View/Scripts/script_resultados.js
@@ -48,27 +48,115 @@ function obtenerResultados(){
 
         duracion = data.Duracion[0].DuracionItem;
 
-        /*Aqui es donde vas a llamar a las funciones para graficar
-        le pasas como parametro los arreglos y ya los puedes usar*/
-        escrbirDropdown(empresasNombre);
+        mostrarDatosEntrada(universidad, licenciatura);
+        graficarDatosEmpresas(empresasNombre, empresasPorcentaje);
+        graficarDatosRoles(rolesNombre, rolesPorcentaje);
+        mostrarDuracion(duracion);
     })
     .catch(error => console.error('Error:', error));
     localStorage.clear();
 }
 
-/*Este es un ejemplo de una funcion donde se usan los arreglos
-que necesitas, la vas a borrar y vas a poner las funciones para
-las graficas*/
-function escrbirDropdown(arregloDatos){
+function mostrarDatosEntrada(universidad, carrera){
+    document.getElementById("university-chosen").innerHTML = "Universidad: " + universidad;
+    document.getElementById("carreer-chosen").innerHTML = "Carrera: " + carrera;
+    document.getElementById("q-companies").innerHTML = "¿Qué empresas contratan egresados de la carrera de " + carrera + "?";
+    document.getElementById("q-roles").innerHTML = "¿Qué puestos han ocupado los egresados de " + carrera + "?";
+    document.getElementById("q-duration").innerHTML = "¿Cuál es la duración promedio de los egresados de " + carrera + " en el que duran en un trabajo?";
+}
 
-    var dropdownPrueba = document.getElementById("opciones")
-    dropdownPrueba.innerHTML = "";
+function graficarDatosEmpresas(empresasNombre, empresasPorcentaje){
+    var ctx = document.getElementById('companiesChart').getContext('2d');
+    var longitud = empresasNombre.length;
+    var colores = [];
+    var empresasMostradas = [];
+    var porcentajesMostrados = [];
+    var i;
 
-    arregloDatos.forEach(dato => {
-        var option = document.createElement("option");
-        option.value = dato;
-        option.text = dato;
-        dropdownPrueba.add(option);
+    if(longitud > 15){
+        longitud *= 0.10;
+    }
+
+    for(i = 0; i < longitud; i++){
+        colores.push(generarColorAleatorio());
+        empresasMostradas.push(empresasNombre[i]);
+        porcentajesMostrados.push(empresasPorcentaje[i]);
+    }
+
+    var data = {
+        labels: empresasMostradas,
+        datasets: [{
+            data: porcentajesMostrados,
+            backgroundColor: colores
+        }]
+    };
+
+    var options = {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: "Porcentaje de egresados contratados por cada empresa"
+        }
+    };
+
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
     });
+}
 
+function graficarDatosRoles(rolesNombre, rolesPorcentaje){
+    var ctx = document.getElementById('rolesChart').getContext('2d');
+    var longitud = rolesNombre.length;
+    var colores = [];
+    var rolesMostrados = [];
+    var porcentajesMostrados = [];
+    var i;
+
+    if(longitud > 15){
+        longitud *= 0.10;
+    }
+
+    for(i = 0; i < longitud; i++){
+        colores.push(generarColorAleatorio());
+        rolesMostrados.push(rolesNombre[i]);
+        porcentajesMostrados.push(rolesPorcentaje[i]);
+    }
+
+    var data = {
+        labels: rolesMostrados,
+        datasets: [{
+            data: porcentajesMostrados,
+            backgroundColor: colores
+        }]
+    };
+
+    var options = {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: "Roles ocupados por egresados"
+        }
+    };
+
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    });
+}
+
+function mostrarDuracion(duracion){
+    document.getElementById("duration").innerHTML = "La duracion promedio que un egresado permanece en un puesto es de " + duracion + " meses.";
+}
+
+function generarColorAleatorio(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    var colorHexadecimal = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+
+    return colorHexadecimal;
 }
